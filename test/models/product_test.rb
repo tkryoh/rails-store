dbed4e092fb2162e84092df1b55e5507ea4ce316
jaysekobo <tkryoh@gmail.com>
2025-11-23 21:22:04 +0900

    テストの追加

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include ActionMailer::TestHelper
+
+  test "sends email notifications when back in stock" do
+    product = products(:tshirt)
+
+    # 製品を在庫切れにする
+    product.update(inventory_count: 0)
+
+    assert_emails 2 do
+      product.update(inventory_count: 99)
+    end
+  end
 end
