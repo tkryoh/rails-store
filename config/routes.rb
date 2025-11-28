449ffce90b9a208131ce5e6a2e9339d59271f3a8
jaysekobo <tkryoh@gmail.com>
2025-11-28 22:33:08 +0900

    ユーザーのプロフィール（名前のみ）を編集する機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,8 @@ Rails.application.routes.draw do
 
   namespace :settings do
     resource :password, only: [ :show, :update ]
+    resource :profile, only: [ :show, :update ]
+
+    root to: redirect("/settings/profile")
   end
 end
