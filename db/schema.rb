bf7c1f3103797403047def9c4619d4026bc2726b
jaysekobo <tkryoh@gmail.com>
2025-12-03 12:45:27 +0900

    メールアドレスの更新機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_28_122214) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_03_011425) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
@@ -80,6 +80,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_28_122214) do
     t.string "first_name"
     t.string "last_name"
     t.string "password_digest", null: false
+    t.string "unconfirmed_email"
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
