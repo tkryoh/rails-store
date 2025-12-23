3bb5a7803a136b7ceb6b096ec428469761de40ec
tkryoh <tkryoh@gmail.com>
2025-12-23 13:42:00 +0900

    管理画面に購読者の表示を追加

--- config/routes.rb
+++ config/routes.rb
@@ -43,6 +43,7 @@ Rails.application.routes.draw do
     resources :products
     resources :users
     resources :wishlists
+    resources :subscribers
 
     root to: redirect("/store/products")
   end
